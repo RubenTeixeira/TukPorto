ce7b0d77692f9ae14f41a19be070add8b6a57ad2
Pedro-Abreu <1131219@isep.ipp.pt>
2016-10-02 00:48:12 +0100

    Projeto na plataforma Visual Studio 2015. Contem ficheiro .css , pagina web e .js Funçao para fazer as tabs a partir da api dos sensors, mas as tabs nao tem css por isso aparecem em texto.

---
+++ ProjetoARQSI/ProjetoARQSI/src/SmartCity.js
@@ -0,0 +1,51 @@
+﻿var sensors = "http://phpdev2.dei.isep.ipp.pt/~nsilva/smartcity/sensores.php";
+var xmlHttpObj;
+
+function listSensors() {
+
+    xmlHttpObj = createXmlHttpRequestObject();
+
+    if (xmlHttpObj) {
+
+        xmlHttpObj.open("GET", sensors, true);
+        xmlHttpObj.onreadystatechange = stateHandler;
+        xmlHttpObj.send(null);
+
+    }
+}
+
+
+function stateHandler() {
+
+    if (xmlHttpObj.readyState == 4 && xmlHttpObj.status == 200) {
+        var doc = xmlHttpObj.responseXML;
+        createTabs(doc);
+
+    }
+
+}
+
+function createXmlHttpRequestObject() {
+    return new XMLHttpRequest();
+}
+
+function createTabs(doc) {
+    var elementDiv = document.getElementById("widget_vertical");
+    var sensorsList = doc.getElementsByTagName("nome");
+    var div = document.createElement("div");
+    var tab = document.createElement('ul');
+    //div.setAttribute("class", "sidebarrightside");
+    //tab.setAttribute("class", "sidebarmenu");
+    for (i = 0; i < sensorsList.length; i++) {
+        var li = document.createElement('li');
+        var text = document.createTextNode(sensorsList[i].childNodes[0].nodeValue);
+        //li.setAttribute("class", "sidebarmenulink");
+        li.appendChild(text);
+        tab.appendChild(li);
+
+    }
+    div.appendChild(tab);
+    elementDiv.appendChild(div);
+
+
+}
