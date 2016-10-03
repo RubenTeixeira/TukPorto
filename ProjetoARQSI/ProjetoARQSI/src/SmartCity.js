c6f4483ae00f630a87020851d0d00cb8a33004eb
1130383 <1130383@isep.ipp.pt>
2016-10-03 17:39:27 +0100

    Sidebar's tags are now being build according to server info. Added: method that loads Sidebar's left tags dynamic load according to xml tree. Edit: CSS classes. ToChange: instead of using references, we'll be using buttons in Sidebar's left side.

--- ProjetoARQSI/ProjetoARQSI/src/SmartCity.js
+++ ProjetoARQSI/ProjetoARQSI/src/SmartCity.js
@@ -2,50 +2,69 @@
 var xmlHttpObj;
 
 function listSensors() {
-
     xmlHttpObj = createXmlHttpRequestObject();
 
     if (xmlHttpObj) {
-
         xmlHttpObj.open("GET", sensors, true);
         xmlHttpObj.onreadystatechange = stateHandler;
         xmlHttpObj.send(null);
-
     }
 }
 
 
 function stateHandler() {
-
     if (xmlHttpObj.readyState == 4 && xmlHttpObj.status == 200) {
-        var doc = xmlHttpObj.responseXML;
-        createTabs(doc);
-
+        var xmlDoc = xmlHttpObj.responseXML;
+        createTabs(xmlDoc);
     }
-
 }
 
 function createXmlHttpRequestObject() {
     return new XMLHttpRequest();
 }
 
-function createTabs(doc) {
-    var elementDiv = document.getElementById("widget_vertical");
-    var sensorsList = doc.getElementsByTagName("nome");
-    var div = document.createElement("div");
-    var tab = document.createElement('ul');
-    //div.setAttribute("class", "sidebarrightside");
-    //tab.setAttribute("class", "sidebarmenu");
+function createTabs(xmlDoc) {   
+    var sensor = xmlDoc.getElementsByTagName("sensores")[0];
+    var sensorsList = sensor.childNodes;
+    var p = 0;
+    //var sensorsList = xmlDoc.getElementsByTagName("nome");
+    var div = document.getElementById("sidebarleftside"); //retrieve sidebars left side
+    var description = [];
+    var list = document.createElement('ul'); //list
+    list.className = "sidebarmenu";
     for (i = 0; i < sensorsList.length; i++) {
-        var li = document.createElement('li');
-        var text = document.createTextNode(sensorsList[i].childNodes[0].nodeValue);
-        //li.setAttribute("class", "sidebarmenulink");
-        li.appendChild(text);
-        tab.appendChild(li);
-
+        if (sensorsList[i].nodeType === 1) { //only Elements Nodes
+            var li = document.createElement('li');
+            li.className = "sidebarmenulink";
+            var a = document.createElement('a');
+            description[p] = sensorsList[i].childNodes[1].childNodes[0].nodeValue;
+            var text = document.createTextNode(description[p]);
+            var ref = (p + 1).toString();
+            var textref = "#" + ref + "sensor";
+            a.href = textref; //append id
+            a.id = textref; //append reference
+            a.appendChild(text);
+            p++;
+            li.appendChild(a);
+            list.appendChild(li);
+        } else {
+            //do nothing.
+        }
     }
-    div.appendChild(tab);
-    elementDiv.appendChild(div);
+    div.appendChild(list);
+    createFirstSensorTab();
+    createSecondSensorTab();
+}
 
-
+function createFirstSensorTab() {
+    var widget = document.getElementById("widget_vertical");
+    /*new division*/
+    var div = document.createElement("div");
+    div.className = "sidebarrightside";
+    div.id = "1sensor";
+    var p = document.createElement("p");
+    var text = document.createTextNode("Preferências");
+    p.appendChild(text);
+    div.appendChild(p);
+    widget.appendChild(div);
 }
