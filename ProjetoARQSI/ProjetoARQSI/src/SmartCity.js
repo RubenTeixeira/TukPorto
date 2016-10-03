1ea816e095d73fa38dfb621a2b73661bbc9766c8
1130383 <1130383@isep.ipp.pt>
2016-10-03 19:40:01 +0100

    SideButtons implemented. Added: CSS code for side buttons. Edited: instead of using ul and li tags, we're using buttons that are being directly placed in the left side division of the sidebar through javascript code.

--- ProjetoARQSI/ProjetoARQSI/src/SmartCity.js
+++ ProjetoARQSI/ProjetoARQSI/src/SmartCity.js
@@ -27,44 +27,25 @@ function createTabs(xmlDoc) {
     var sensor = xmlDoc.getElementsByTagName("sensores")[0];
     var sensorsList = sensor.childNodes;
     var p = 0;
-    //var sensorsList = xmlDoc.getElementsByTagName("nome");
     var div = document.getElementById("sidebarleftside"); //retrieve sidebars left side
     var description = [];
-    var list = document.createElement('ul'); //list
-    list.className = "sidebarmenu";
     for (i = 0; i < sensorsList.length; i++) {
         if (sensorsList[i].nodeType === 1) { //only Elements Nodes
-            var li = document.createElement('li');
-            li.className = "sidebarmenulink";
-            var a = document.createElement('a');
+            var button = document.createElement("button");
+            button.type = "button";
             description[p] = sensorsList[i].childNodes[1].childNodes[0].nodeValue;
             var text = document.createTextNode(description[p]);
             var ref = (p + 1).toString();
             var textref = "#" + ref + "sensor";
-            a.href = textref; //append id
-            a.id = textref; //append reference
-            a.appendChild(text);
+            button.id = textref; //append reference
+            button.className = "sidebarbutton";
+            button.appendChild(text);
             p++;
-            li.appendChild(a);
-            list.appendChild(li);
+            div.appendChild(button);
         } else {
             //do nothing.
         }
     }
-    div.appendChild(list);
     createFirstSensorTab();
     createSecondSensorTab();
-}
-
-function createFirstSensorTab() {
-    var widget = document.getElementById("widget_vertical");
-    /*new division*/
-    var div = document.createElement("div");
-    div.className = "sidebarrightside";
-    div.id = "1sensor";
-    var p = document.createElement("p");
-    var text = document.createTextNode("Preferências");
-    p.appendChild(text);
-    div.appendChild(p);
-    widget.appendChild(div);
 }
