b6dc6ab2238a058649a30c163e7ba5b16d42464e
Pedro-Abreu <1131219@isep.ipp.pt>
2016-11-11 12:26:55 +0000

    Visita initial commit

---
+++ ProjetoARQSI/Datum/Utils/WebApiHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datum.Utils
+{
+    public class WebApiHttpClient
+    {
+        public const string WebApiBaseAddress = "http://localhost:4168/";
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(WebApiBaseAddress);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new
+            System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+
+}
