f6fc05514d649124a4b44f20ce1595e826caf43e
Ruben Teixeira <1140780@isep.ipp.pt>
2016-11-13 23:25:24 +0000

    Fixed GetMetereologiaByPeriod logic

--- ProjetoARQSI/Cancela/Controllers/SensoresController.cs
+++ ProjetoARQSI/Cancela/Controllers/SensoresController.cs
@@ -1,5 +1,8 @@
 ﻿using Cancela.Helpers;
+using Datum.DAL;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -7,9 +10,10 @@ using System.Web.Http.Cors;
 
 namespace Cancela.Controllers
 {
+    [RoutePrefix("api/Sensores")]
     public class SensoresController : ApiController
     {
-
+        private DatumContext db = new DatumContext();
 
         // GET api/sensores
         [EnableCors(origins: "*", headers: "*", methods: "*")]
@@ -32,17 +36,77 @@ namespace Cancela.Controllers
                 meteoFacetas.addFaceta("2", "DataHoraLeitura", "Data", "contínuo", "data", "data");
                 meteoFacetas.addFaceta("3", "Temp", "Temperatura", "contínuo", "numérico", "temperatura");
                 meteoFacetas.addFaceta("4", "Vento", "Vento", "contínuo", "numérico", "vento");
-                meteoFacetas.addFaceta("5", "Humidade", "contínuo", "discreto", "numérico", "humidade");
-                meteoFacetas.addFaceta("6", "Pressao", "contínuo", "discreto", "numérico", "pressão");
+                meteoFacetas.addFaceta("5", "Humidade", "Humidade", "contínuo", "numérico", "humidade");
+                meteoFacetas.addFaceta("6", "Pressao", "Pressao", "contínuo", "numérico", "pressão");
                 meteoFacetas.addFaceta("7", "NO", "NO", "contínuo", "numérico", "NO");
                 meteoFacetas.addFaceta("8", "NO2", "NO2", "contínuo", "numérico", "NO2");
                 meteoFacetas.addFaceta("9", "CO2", "CO2", "contínuo", "numérico", "CO2");
                 return this.Request.CreateResponse(HttpStatusCode.OK, meteoFacetas);
-            } else
+            }
+            else
             {
 
                 return this.Request.CreateResponse(HttpStatusCode.NotFound);
             }
+        }
+
+        // GET api/sensores/DiscreteValues?sensorId=100&facetaId=1
+        //[Route("sensores/DiscreteValues/{sensorId}/{facetaId}")]
+        [Route("DiscreteValues")]
+        [HttpGet]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public HttpResponseMessage DiscreteValues(int sensorId, int facetaId)
+        {
+            var list = new List<Object>();
+            switch (facetaId)
+            {
+                case 1:
+                    var locals = db.Locals;
+                    foreach (var item in locals) { list.Add(item.Nome); };
+                    break;
+                default:
+                    return this.Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return this.Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+
+        // GET api/sensores/MinValue?sensorId=100&facetaId=1
+        //[Route("sensores/MinValue/{sensorId}/{facetaId}")]
+        [Route("MinValue")]
+        [HttpGet]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public HttpResponseMessage MinValue(int sensorId, int facetaId)
+        {
+
+            double min = 0;
+
+            switch (facetaId)
+            {
+                case 3:
+                    min = db.Metereologias.Max(m => m.Temp);
+                    break;
+                case 4:
+                    min = db.Metereologias.Max(m => m.Vento);
+                    break;
+                case 5:
+                    min = db.Metereologias.Max(m => m.Humidade);
+                    break;
+                case 6:
+                    min = db.Metereologias.Max(m => m.Pressao);
+                    break;
+                case 7:
+                    min = db.Metereologias.Max(m => m.NO);
+                    break;
+                case 8:
+                    min = db.Metereologias.Max(m => m.NO2);
+                    break;
+                case 9:
+                    min = db.Metereologias.Max(m => m.CO2);
+                    break;
+                default:
+                    return this.Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            return this.Request.CreateResponse(HttpStatusCode.OK, min);
         }
     }
 }
